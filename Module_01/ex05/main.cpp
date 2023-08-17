e035fff08455a5590e9083194ebc65538baf5107
begopla <begonapr@gmail.com>
2023-08-17 16:47:37 +0400

    ex05

---
+++ Module_01/ex05/main.cpp
@@ -0,0 +1,16 @@
+
+#include "Karen.hpp"
+
+int main( void )
+{
+	Karen test;
+
+	test.complain("debug");
+	test.complain("info");
+	test.complain("warning");
+	test.complain("error");
+	/*wrong examples:*/
+	test.complain("xxxxxxxxx");
+	test.complain("dEbuG");
+	test.complain("Info");
+}
